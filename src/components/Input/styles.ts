8d6f0295c2c6dcf67150a608dc445ab057b8ac20
Tiago Rodrigues <tiagoadriano.rodrigues@gmail.com>
2021-01-02 22:15:52 -0300

    Add yup to the project

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,12 +1,18 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
   background: #232129;
   border-radius: 10px;
-  border: 2px solid #232129;
-
   width: 100%;
+
+  border: 2px solid #232129;
   color: #f4ede8;
+
   display: flex;
   align-items: center;
   color: #666360;
@@ -15,6 +21,21 @@ export const Container = styled.div`
   & + div {
     margin-top: 8px;
   }
+
+  transition: 0.2s ease-in all;
+
+  ${props =>
+    props.isFocused &&
+    css`
+      border: 2px solid #ff9000;
+      color: #ff9000;
+    `}
+
+  ${props =>
+    props.isFilled &&
+    css`
+      color: #ff9000;
+    `}
 
   input {
     padding: 16px;
--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
 
 import { Container, Content, Background } from './styles';
 import logoImg from '../../assets/images/logo.svg';
@@ -7,33 +9,53 @@ import logoImg from '../../assets/images/logo.svg';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-const Register: React.FC = () => (
-  <Container>
-    <Background />
-    <Content>
-      <img src={logoImg} alt="GoBarber" />
+const Register: React.FC = () => {
+  const handleSubmit = useCallback(async (data: void) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Digite seu nome'),
+        email: Yup.string()
+          .required('Digite seu e-mail')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().min(6, 'No mínimo 6 digitos'),
+      });
 
-      <form action="">
-        <h1>Faça seu cadastro</h1>
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
 
-        <Input name="name" type="text" placeholder="Nome" icon={FiUser} />
-        <Input name="email" type="text" placeholder="E-mail" icon={FiMail} />
-        <Input
-          name="password"
-          type="password"
-          placeholder="Senha"
-          icon={FiLock}
-        />
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
 
-        <Button type="submit">Entrar</Button>
-      </form>
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu cadastro</h1>
 
-      <a href="forgot" title="Cadastre-se">
-        <FiArrowLeft />
-        Voltar para o login
-      </a>
-    </Content>
-  </Container>
-);
+          <Input name="name" type="text" placeholder="Nome" icon={FiUser} />
+          <Input name="email" type="text" placeholder="E-mail" icon={FiMail} />
+          <Input
+            name="password"
+            type="password"
+            placeholder="Senha"
+            icon={FiLock}
+          />
+
+          <Button type="submit">Entrar</Button>
+        </Form>
+
+        <a href="forgot" title="Cadastre-se">
+          <FiArrowLeft />
+          Voltar para o login
+        </a>
+      </Content>
+    </Container>
+  );
+};
 
 export default Register;
