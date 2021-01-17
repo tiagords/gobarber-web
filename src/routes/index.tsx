c60b6901dadb294305f3b86538ce3372d68797cb
Tiago Rodrigues <tiagoadriano.rodrigues@gmail.com>
2021-01-17 13:57:37 -0300

    Add Authenticated Routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 
 import Login from '../pages/Login';
 import Register from '../pages/Register';
+import Dashboard from '../pages/Dashboard';
+import Route from './Route';
 
 const Routes: React.FC = () => {
   return (
     <Switch>
       <Route path="/" exact component={Login} />
       <Route path="/register" component={Register} />
+
+      <Route path="/dashboard" component={Dashboard} isPrivate />
     </Switch>
   );
 };
